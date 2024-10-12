1b9a0c1096d49a408df0914b2e4d81ac3cce5446
Abidur Rahman <70755047+MrAbidAkash@users.noreply.github.com>
2024-10-12 17:38:48 +0000

    fix: clerk bugs

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://actual-glider-21.clerk.accounts.dev",
+      domain: "https://robust-guppy-40.clerk.accounts.dev",
       applicationID: "convex",
     }
   ]
