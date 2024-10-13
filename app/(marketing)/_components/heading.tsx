d193927516923c7fdb8c2079ecf22f0aa3bed97b
Abidur Rahman <70755047+MrAbidAkash@users.noreply.github.com>
2024-10-13 05:47:58 +0000

    fixed

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -14,11 +14,11 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
+        Unifying Your Ideas, Documents, and Plans in One Place. Welcome to <span className="underline">Ghotion</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Jotion is the connected workspace where <br />
-        better, faster work happens.
+        Ghotion is the dynamic workspace where <br />
+        smarter, quicker work comes to life.
       </h3>
       {isLoading && (
         <div className="w-full flex items-center justify-center">
