5d277a332f21672f7d80356fa92775f6b72dfef1
Abidur Rahman <70755047+MrAbidAkash@users.noreply.github.com>
2024-10-13 05:59:33 +0000

    fixed

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -14,11 +14,11 @@ export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-        Unifying Your Ideas, Documents, and Plans in One Place. Welcome to <span className="underline">Ghotion</span>
+        Write. Plan. Collaborate. Welcome to <span className="underline">Ghotion</span>
       </h1>
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-        Ghotion is the dynamic workspace where <br />
-        smarter, quicker work comes to life.
+        In Notion, work feels better (and gets done faster).<br />
+        With a little help from AI.
       </h3>
       {isLoading && (
         <div className="w-full flex items-center justify-center">
